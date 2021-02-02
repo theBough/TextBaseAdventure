f9cfa0b96d72a262e751df9cafb2eaa3fb6c0ad5
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-02-02 09:45:03 -0600

    Update ScriptSecondAlpha.js

--- ScriptSecondAlpha.js
+++ ScriptSecondAlpha.js
@@ -1,18 +1,34 @@
 var msg = document.getElementById("output");
+var isSlap = false;
 
-function gameStart(){
+function gameStart() {
   var input = document.getElementById("myText").value;
-  if (input == 'h'){
-    msg.innerHTML += ">>>>" + input + "</br>" + help;
-  }
-  
+
+  if (isSlap) {
+    slapOn();
+  } else {
+    switch (input) {
+      case "h":
+        msg.innerHTML += ">>>>" + input + "</br>" + help;
+        break;
+      case "s":
+        msg.innerHTML += ">>>>" + input + "</br>Who would you like to slap?";
+        isSlap = true;
+        break;
+      default:
+        msg.innerHTML += ">>>>" + input + "</br>Sorry I don't understand this command";
+        break;
+    }//end switch
+  }//end if
 }
 
-help = '***************************</br>\
+var help = '***************************</br>\
 | Here are the commands for the game:</br>\
 | h - help</br>\
 | i - inspect an item</br>\
 | u - use an item</br>\
 | m - to see the map</br>\
 | c - to see what you ar carrying</br>\
-***************************';
+| s - to slap someone.</br>\
+  *************************** ';
+  
