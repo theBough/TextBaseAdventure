c908fe1afe0b0516e83ad244c7c6338cbe85f6e0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-02-02 09:13:41 -0600

    Update ScriptSecondAlpha.js

--- ScriptSecondAlpha.js
+++ ScriptSecondAlpha.js
@@ -1,6 +1,18 @@
 var msg = document.getElementById("output");
 
 function gameStart(){
-  msg.innerHTML = "I hope you enjoy my game.";
-
+  var input = document.getElementById("myText").value;
+  if (input == 'h'){
+    msg.innerHTML += ">>>>" + input + "</br>" + help;
+  }
+  
 }
+
+help = '***************************</br>\
+| Here are the commands for the game:</br>\
+| h - help</br>\
+| i - inspect an item</br>\
+| u - use an item</br>\
+| m - to see the map</br>\
+| c - to see what you ar carrying</br>\
+***************************';
